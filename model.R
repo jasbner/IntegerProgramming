466a396921ba13514589b245cbb434d037e6f033
Jason Brown <brown.jason@epa.gov>
2019-04-18 14:01:01 -0400

    added constraint for 6 ppl per team

--- model.R
+++ model.R
@@ -4,19 +4,22 @@ library(lpSolve)
 df <- data_frame(avg_points = sample(5:45,1000, replace = T),
                  cost = sample(3:45,1000, replace = T),
                  position = sample(c("P","C","1B","2B","3B","SS","OF"),1000, replace = T),
+                 team = sample(LETTERS,1000, replace = T),
                  one = 1) %>% mutate(id = row_number())
 
 obj <- df$avg_points #set the objective function (what we want to maximize)
 # set the constraint rows. contrary to tidy dataframes, each row is what you are summing over
-con <- rbind(t(model.matrix(~ position + 0,df)), cost = df$cost) 
+con <- rbind(t(model.matrix(~ position + 0,df)), cost = df$cost, t(model.matrix(~ team + 0, df)) )
 
 #view the constraints
 rownames(con)
 #set the constraint values
-rhs <- c(1,1,1,1,3,2,1,200) #exactly 3 outfielders 2 pitchers and 1 of everything else at a cost less than 200
+rhs <- c(1,1,1,1,3,2,1,200, #exactly 3 outfielders 2 pitchers and 1 of everything else at a cost less than 200
+         rep(6,26) #max number from any team is 6
+         ) 
 
 #set the direction of the constraints
-dir <- c("=","=","=","=","=","=","=","<=")
+dir <- c("=","=","=","=","=","=","=","<=",rep("<=",26))
 
 result <- lp("max",obj,con,dir,rhs,all.bin = TRUE)
 
